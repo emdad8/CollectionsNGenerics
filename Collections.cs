3cc998beddb13744b33c060e15407581973d0e52
Emdadul Haque <emdad8@gmail.com>
2023-01-08 14:02:32 +0600

    Add files via upload

---
+++ Collections.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndCollections
+{
+    class Collections
+    {
+        // methods for enum
+
+
+
+
+
+        // methods for list
+
+
+
+
+        // methods for Dictionary
+
+
+
+
+        // methods for set
+    }
+}
